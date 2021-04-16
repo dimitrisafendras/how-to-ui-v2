7d061629c10e94ca4a8977d8efcd34ae81742031
dimitrisafendras <dimitrisafendras@gmail.com>
2021-04-16 13:13:36 +0300

    refactor rename classes

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,10 +5,10 @@ export const Main = ({ className }) =>
 	<div className={`Main ${className}`}>
 		<div className="Main__layout">
 			{/*Discuss if instead spancol/row 2*/}
-			<Card className='Main-layout__bigCard' bigCard/>
-			<Card className='Main-layout__smallCard1'/>
-			<Card className='Main-layout__smallCard2'/>
-			<Card className='Main-layout__smallCard3'/>
-			<Card className='Main-layout__smallCard4'/>
+			<Card className='Main__bigCard' bigCard/>
+			<Card className='Main__smallCard1'/>
+			<Card className='Main__smallCard2'/>
+			<Card className='Main__smallCard3'/>
+			<Card className='Main__smallCard4'/>
 		</div>
 	</div>
