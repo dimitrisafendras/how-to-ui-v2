0d80015388adde6bc66cf5196fb48269170f44ed
kostispodaras <kostis.podaras@agileactors.com>
2021-06-16 23:17:27 +0300

    wip

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,17 +3,16 @@ import { Logo, BurgerMenu } from './images';
 
 export const Header = ({ className }) => (
   <div className={`Header ${className}`}>
-    <div className="leftSection">
+    <div className="Header__leftSection">
       <a href="#">Toys</a>
       <a href="#">Our story</a>
     </div>
 
-    <div>
-      <img src={Logo} alt="logo" className="logo" />
-      <img src={BurgerMenu} alt="menu" className="burgerMenu" />
-    </div>
+    <img src={Logo} alt="logo" className="Header__logo" />
 
-    <div className="rightSection">
+    {/* <img src={BurgerMenu} alt="menu" className="burgerMenu" /> */}
+
+    <div className="Header__rightSection">
       <a href="#">Shop</a>
       <a href="#">Contact</a>
     </div>
